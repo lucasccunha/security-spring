5275fb40bd8c152cc4ae24cfc300f392aaad7ee2
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-24 12:25:12 -0300

    configuring users

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,11 +2,13 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +24,17 @@ public class ConfiguracoesSeguranca {
                 .password("{noop}maria123")
                 .build();
         return new InMemoryUserDetailsManager(usuario1, usuario2);
+    }
 
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(req -> {
+            req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+            req.anyRequest().authenticated();
+        })
+                .formLogin(form -> form.loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll())
+                .build();
     }
 }
