eaf8e8308cabc5904286d08b373f6b756755e379
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-24 11:23:17 -0300

    configuring users

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -1,9 +1,27 @@
 package med.voll.web_application.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
+    @Bean
+    public UserDetailsService dadosUsuariosCadastrados() {
+        UserDetails usuario1 = User.builder()
+                .username("lucas@email.com")
+                .password("{noop}lucas123")
+                .build();
+        UserDetails usuario2 = User.builder()
+                .username("maria@email.com")
+                .password("{noop}maria123")
+                .build();
+        return new InMemoryUserDetailsManager(usuario1, usuario2);
+
+    }
 }
