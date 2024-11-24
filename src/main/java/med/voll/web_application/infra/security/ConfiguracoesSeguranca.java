1944260bb95d8190915dce0359044085d1927a75
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-24 14:47:51 -0300

    CSRF

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,6 +2,7 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -41,6 +42,7 @@ public class ConfiguracoesSeguranca {
                         .permitAll())
                 .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
                         .alwaysRemember(true))
+                .csrf(Customizer.withDefaults())
                 .build();
     }
 }
