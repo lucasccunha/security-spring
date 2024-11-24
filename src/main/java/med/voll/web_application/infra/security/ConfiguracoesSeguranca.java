f71171101b5e67054a902c90ddd6850a2e141882
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-24 11:12:10 -0300

    first commit

---
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -0,0 +1,9 @@
+package med.voll.web_application.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesSeguranca {
+}
