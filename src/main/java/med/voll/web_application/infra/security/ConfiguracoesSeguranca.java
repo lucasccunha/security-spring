caf0d0e3b9e87049fe931196e43c86495b35a308
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-24 14:18:45 -0300

    remember me

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -39,6 +39,8 @@ public class ConfiguracoesSeguranca {
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout")
                         .permitAll())
+                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
+                        .alwaysRemember(true))
                 .build();
     }
 }
