78b6a95f4f251c0b22c61a838d9dcd2f3879871c
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-25 12:59:01 -0300

    remember me

--- src/main/java/med/voll/web_application/domain/usuario/Usuario.java
+++ src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -1,2 +1,14 @@
-package med.voll.web_application.domain.usuario;public class Usuario {
+package med.voll.web_application.domain.usuario;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
 }
