ac5cddb531916fa1a815c4b602e69b457744d8c7
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-27 22:53:28 -0300

    config login

--- src/main/java/med/voll/web_application/domain/usuario/Usuario.java
+++ src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -1,14 +1,33 @@
 package med.voll.web_application.domain.usuario;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
-@Table(name="usuarios")
-public class Usuario {
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
