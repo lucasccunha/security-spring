1fa467e858cf5cbf16cff005ab7c5079b915370e
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-28 23:00:13 -0300

    Authorizing access to profiles

--- src/main/java/med/voll/web_application/controller/ConsultaController.java
+++ src/main/java/med/voll/web_application/controller/ConsultaController.java
@@ -5,9 +5,11 @@ import med.voll.web_application.domain.RegraDeNegocioException;
 import med.voll.web_application.domain.consulta.ConsultaService;
 import med.voll.web_application.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.web_application.domain.medico.Especialidade;
+import med.voll.web_application.domain.usuario.Usuario;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,7 +25,7 @@ public class ConsultaController {
 
     private final ConsultaService service;
 
-    public ConsultaController(ConsultaService consultaService) {
+    public ConsultaController(ConsultaService consultaService){
         this.service = consultaService;
     }
 
