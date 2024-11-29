f0c3c38482da0f98f540efd5c2d91d5fa19e533e
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-28 23:55:25 -0300

    restricting views

--- src/main/java/med/voll/web_application/domain/consulta/ConsultaService.java
+++ src/main/java/med/voll/web_application/domain/consulta/ConsultaService.java
@@ -3,8 +3,11 @@ package med.voll.web_application.domain.consulta;
 import jakarta.transaction.Transactional;
 import med.voll.web_application.domain.medico.MedicoRepository;
 import med.voll.web_application.domain.paciente.PacienteRepository;
+import med.voll.web_application.domain.usuario.Perfil;
+import med.voll.web_application.domain.usuario.Usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,8 +23,10 @@ public class ConsultaService {
         this.pacienteRepository = pacienteRepository;
     }
 
-    public Page<DadosListagemConsulta> listar(Pageable paginacao) {
-        return repository.findAllByOrderByData(paginacao).map(DadosListagemConsulta::new);
+    public Page<DadosListagemConsulta> listar(Pageable paginacao, Usuario logado) {
+        if (logado.getPerfil() == Perfil.ATENDENTE)
+            return repository.findAllByOrderByData(paginacao).map(DadosListagemConsulta::new);
+        return repository.buscaPersonalizadaConsultas(logado.getId(), paginacao).map(DadosListagemConsulta::new);
     }
 
     @Transactional
